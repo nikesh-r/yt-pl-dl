e555e981878676af862f6352771056edbc251ff3
Nikesh <nikesh.ramnani@gmail.com>
2021-08-08 14:10:33 +0530

    get videos id from api

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
 const axios = require("axios");
+require("dotenv").config();
 
-const app = () => {
-  dataFromApi();
+const MY_KEY = process.env.MY_YT_API_KEY;
+const PLAYLIST_ID = "PLTBwOxolC2B3uRPWtXpNq8l71hsuRx8Zs"; // "PLzMsvNpDYBM7EjR8dgqM_kYfj_MAUAxEA";
+const MAX_RESULT = 5; // Can be between 0 and 50 inclusive
+let videosIdList = [];
+
+const checkNextPage = async (npt) => {
+  let NPT = npt;
+  console.log(NPT);
+  const res = await axios.get(
+    `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${PLAYLIST_ID}&maxResults=${MAX_RESULT}&pageToken=${NPT}&key=${MY_KEY}`
+  );
+  const data = await res.data;
+  // console.log(data);
+  await saveVideos(data);
 };
 
-const dataFromApi = async () => {
+const saveVideos = async (data) => {
+  const numberOfVideos = data.items.length;
+  for (let i = 0; i < numberOfVideos; i++) {
+    videosIdList.push(data.items[i].contentDetails.videoId);
+  }
+  if (data.nextPageToken) {
+    await checkNextPage(data.nextPageToken);
+  }
+};
+
+const getDataFromApi = async () => {
   try {
-    const res = await axios.get("https://catfact.ninja/fact");
-    const fact = await res.data.fact;
-    console.log(fact);
+    const res = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${PLAYLIST_ID}&maxResults=${MAX_RESULT}&key=${MY_KEY}`
+    );
+    const data = await res.data;
+
+    if (data.items.length !== 0) {
+      await saveVideos(data);
+    } else {
+      console.log("No videos in playlist! or All videos saved!");
+      return;
+    }
   } catch (err) {
     console.log(err);
   }
 };
 
+const app = async () => {
+  await getDataFromApi();
+  console.log(videosIdList);
+};
+
 app();
+
+// https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=PLzMsvNpDYBM7EjR8dgqM_kYfj_MAUAxEA&maxResults=5&key=AIzaSyD75QFarE-b4qtyYuWBScZCaY5Fc7LW_cI
