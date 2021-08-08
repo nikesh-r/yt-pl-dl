d0da54955bcb917425319d44289e8e3b3c90297e
Nikesh <nikesh.ramnani@gmail.com>
2021-08-08 22:44:04 +0530

    complete core functionality

--- app.js
+++ app.js
@@ -1,33 +1,81 @@
 const axios = require("axios");
+const { create } = require("domain");
 const fs = require("fs");
+const libDownload = require("./libdownload");
 require("dotenv").config();
 
 const MY_KEY = process.env.MY_YT_API_KEY;
-const PLAYLIST_ID = "PLTBwOxolC2B3uRPWtXpNq8l71hsuRx8Zs"; // "PLzMsvNpDYBM7EjR8dgqM_kYfj_MAUAxEA";
+
+const PLAYLIST_ID = "PLTBwOxolC2B1BODxYx4ZNBc0pWYKRu7RF"; // "PLzMsvNpDYBM7EjR8dgqM_kYfj_MAUAxEA";
+// const PLAYLIST_ID = "PLTBwOxolC2B3uRPWtXpNq8l71hsuRx8Zs"; // "PLzMsvNpDYBM7EjR8dgqM_kYfj_MAUAxEA";
 const MAX_RESULT = 5; // Can be between 0 and 50 inclusive
-let videosIdList = [];
+let videoIDList = [];
+let videoTitleList = [];
 let PLAYLIST_NAME;
 
+function createFolder(playlistID) {
+  // create folder
+  let folderPath = "./" + playlistID;
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+  return folderPath;
+}
+function createOutputPath(folderPath, videoTitle) {
+  // return filename
+  return folderPath + "\\" + videoTitle + ".mkv";
+}
 
-function createFolder(playilstID)
-{
-    // create folder
-    folderPath = "./" + playilstID;
-    if (!fs.existsSync(folderPath))
-    {
-        fs.mkdirSync(folderPath);
+const initializeDB = (dbFilePath) => {
+  try {
+    fs.writeFileSync(dbFilePath, JSON.stringify([], null, 2));
+  } catch (e) {
+    console.log("Cannot write file ", e);
+  }
+};
+
+const initializePlaylist = (playlistArray, playlistId) => {
+  let obj = {};
+  obj.id = playlistId;
+  obj.videos = [];
+  playlistArray.push(obj);
+  return obj;
+};
+
+const storeVideoInDB = (dbFilePath, videoID) => {
+  if (!fs.existsSync(dbFilePath)) {
+    try {
+      initializeDB(dbFilePath);
+    } catch (e) {
+      console.error("Cannot write file ", e);
     }
-    return folderPath
-}
-function createOutputPath(folderPath, videoID)
-{
-    // return filename
-    return folderPath + "\\" + videoID + ".mkv";
-}
+  }
+  let dbContent = fs.readFileSync("config.json");
+  let playlistArray = JSON.parse(dbContent);
+  let playlistExist = false;
+  let playlistObject = {};
+  for (let i = 0; i < playlistArray.length; i++) {
+    playlistObject = playlistArray[i];
+    if (playlistObject.id === PLAYLIST_ID) {
+      playlistExist = true;
+    }
+  }
+  if (playlistExist === false) {
+    playlistObject = initializePlaylist(playlistArray, PLAYLIST_ID);
+  }
+  let storedVideoList = playlistObject.videos;
+
+  const isVideoAlreadyPresent = storedVideoList.includes(videoID);
+  if (isVideoAlreadyPresent) {
+    console.log("Video already present in config");
+  } else {
+    storedVideoList.push(videoID);
+    fs.writeFileSync(dbFilePath, JSON.stringify(playlistArray, null, 2));
+  }
+};
 
 const checkNextPage = async (npt) => {
   let NPT = npt;
-  console.log(NPT);
   const res = await axios.get(
     `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${PLAYLIST_ID}&maxResults=${MAX_RESULT}&pageToken=${NPT}&key=${MY_KEY}`
   );
@@ -39,7 +87,8 @@ const checkNextPage = async (npt) => {
 const saveVideos = async (data) => {
   const numberOfVideos = data.items.length;
   for (let i = 0; i < numberOfVideos; i++) {
-    videosIdList.push(data.items[i].snippet.resourceId.videoId);
+    videoIDList.push(data.items[i].snippet.resourceId.videoId);
+    videoTitleList.push(data.items[i].snippet.title);
   }
   if (data.nextPageToken) {
     await checkNextPage(data.nextPageToken);
@@ -52,7 +101,7 @@ const getDataFromApi = async () => {
       `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${PLAYLIST_ID}&maxResults=${MAX_RESULT}&key=${MY_KEY}`
     );
     const data = await res.data;
-    console.log(data);
+    // console.log(data);
     if (data.items.length !== 0) {
       await saveVideos(data);
     } else {
@@ -64,9 +113,60 @@ const getDataFromApi = async () => {
   }
 };
 
+const isVideoPresentInDb = (dbFilePath, videoID) => {
+  if (!fs.existsSync(dbFilePath)) {
+    return false;
+  }
+
+  let dbContent = fs.readFileSync("config.json");
+  let playlistArray = JSON.parse(dbContent);
+  let playlistExist = false;
+  let playlistObject = {};
+  playlistObject = playlistArray.find((element) => element.id === PLAYLIST_ID);
+  if (playlistObject === null) {
+    console.log(
+      "playlist does not exist - isVideoPresentInDb: " + playlistObject
+    );
+    return false;
+  }
+  // console.log(playlistArray[0][PLAYLIST_ID]);
+  let storedVideoList = playlistObject.videos;
+
+  const isVideoAlreadyPresent = storedVideoList.includes(videoID);
+  if (isVideoAlreadyPresent) {
+    console.log("video already present", videoID);
+    return true;
+  } else {
+    console.log("video not already present", videoID);
+    return false;
+  }
+};
+
 const app = async () => {
   await getDataFromApi();
-  console.log(videosIdList);
+  console.log(PLAYLIST_ID);
+  const folderPath = createFolder(PLAYLIST_ID);
+  videoIDList.forEach((videoID) => {
+    console.log("video id is", videoID);
+    let isVideoPresent = isVideoPresentInDb("./config.json", videoID);
+    if (isVideoPresent === false) {
+      let index = videoIDList.findIndex((element) => element === videoID);
+      videoTitle = videoTitleList[index];
+      const outputPath = createOutputPath(folderPath, videoTitle);
+      libDownload.downloadVideo(videoID, outputPath).then((rc) => {
+        console.log("download response: ", rc);
+        if (rc === 0) {
+          // save to db
+          console.log("before storevideo: " + videoID);
+          storeVideoInDB("./config.json", videoID);
+        } else {
+          console.log("storevideo didnt run");
+        }
+      });
+    } else {
+      console.log("isVideoPresent is true");
+    }
+  });
 };
 
 app();
